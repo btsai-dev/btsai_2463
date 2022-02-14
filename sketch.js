56d23faca2eb427aaf15afcb999194568c7e14aa
Brian T <godonanabt@gmail.com>
2022-02-14 16:29:57 -0600

    v1

--- sketch.js
+++ sketch.js
@@ -1,68 +1,63 @@
-let male_character;
-let female_character;
-let other_character;
-
-let male_spritesheet;
-let female_spritesheet;
-let other_spritesheet;
-
+let bugs = [];
+let count = 10;
+let hitbox = 100;
+let score = 0;
 
 function preload() {
-  male_spritesheet = loadImage("SpelunkyGuy.png");
-  female_spritesheet = loadImage("SpelunkyGirl.png");
-  other_spritesheet = loadImage("SpelunkyOther.png");
+  bug_spritesheet = loadImage("SpelunkyGuy.png");
+}
+
+function mouseClicked() {
+  for (i = 0; i < count; i++) {
+    bugs[i].kill();
+  }
+}
+
+function mousePressed() {
+  for (i = 0; i < count; i++) {
+    bugs[i].squish();
+  }
+}
+
+function mouseDragged() {
+  for (i = 0; i < count; i++) {
+    bugs[i].squish();
+  }
 }
 
 function setup() {
   createCanvas(1200, 600);
   imageMode(CENTER);
 
-  let random_1x = int(random(100, 1100));
-  let random_1y = int(random(100, 500));
-  let random_2x = int(random(100, 1100));
-  let random_2y = int(random(100, 500));
-  let random_3x = int(random(100, 1100));
-  let random_3y = int(random(100, 500));
-
-  male_character = new Character(male_spritesheet, random_1x, random_1y)
-  female_character = new Character(female_spritesheet, random_2x, random_2y)
-  other_character = new Character(other_spritesheet, random_3x, random_3y)
-}
-
-function keyPressed() {
-  if (keyCode == RIGHT_ARROW) {
-    male_character.go(1);
-    female_character.go(1);
-    other_character.go(1);
+  for (i=0; i<count; i++) {
+    bugs[i] = new Bug(
+      bug_spritesheet, 
+      random(100, 1100), 
+      random(100, 500), 
+      5,
+      random([-1, 1])
+    )
   }
-  else if (keyCode == LEFT_ARROW) {
-    male_character.go(-1);
-    female_character.go(-1);
-    other_character.go(-1);
-  }
-}
-
-function keyReleased() {
-  male_character.stop();
-  female_character.stop();
-  other_character.stop();
 }
 
 function draw() {
-  background(255, 255, 255);
-  male_character.draw();
-  female_character.draw();
-  other_character.draw();
+  background(200, 200, 200);
+  for (i=0; i<count; i++) {
+    bugs[i].draw();
+  }
 }
 
-class Character {
-  constructor(spriteSheet, x, y) {
+class Bug {
+  constructor(spriteSheet, x, y, speed, move) {
     this.spriteSheet = spriteSheet;
     this.sx = 0;
-    this.x = x;
-    this.y = y
-    this.move = 0;
-    this.face_dir = 1;
+    this.x = int(x);
+    this.y = int(y);
+    this.move = move;
+    this.speed = speed;
+    this.face_dir = move;
+    this.dead = false;
+    this.squished = false;
   }
 
   draw() {
@@ -70,16 +65,31 @@ class Character {
     translate(this.x, this.y);
     scale (this.face_dir, 1);
     
-    if (this.move == 0) {
-      image(this.spriteSheet, 0, 0, 200, 200, 0, 0, 80, 80);
+    if (this.dead) {
+      image(this.spriteSheet, 0, 0, hitbox, hitbox, 80*9, 0, 90, 80);
+    }
+    else if (this.squished) {
+      image(this.spriteSheet, 0, 0, hitbox, hitbox, 80, 80, 80, 80);
     }
     else {
-      image(this.spriteSheet, 0, 0, 200, 200, 80 * (this.sx + 1), 0, 80, 80);
+      image(this.spriteSheet, 0, 0, hitbox, hitbox, 80 * (this.sx + 1), 0, 80, 80);
     }
     if (frameCount % 8 == 0) {
       this.sx = (this.sx + 1) % 8;
     }
-    this.x += 2 * this.move;
+    this.x += this.speed * this.move;
+
+    if (!this.squished && !this.dead)
+    {
+      if (this.x < hitbox/2-10) {
+        this.move = 1;
+        this.face_dir = 1;
+      }
+      else if (this.x > width-hitbox/2+10) {
+        this.move = -1;
+        this.face_dir = -1;
+      }
+    }
     pop();
   }
 
@@ -91,5 +101,27 @@ class Character {
 
   stop() {
     this.move = 0;
+  }
+
+  squish() {
+    if (!this.dead) {
+      if (mouseX > this.x - hitbox/2-10 && mouseX < this.x + hitbox/2-10 && mouseY > this.y - hitbox/2-10 && mouseY < this.y + hitbox/2-10) {
+        this.stop();
+        this.squished = true;
+      }
+      else{
+        this.go(this.face_dir);
+        this.squished = false;
+      }
+    }
+  }
+
+  kill() {
+    if (!this.dead) {
+      if (mouseX > this.x - hitbox/2-10 && mouseX < this.x + hitbox/2-10 && mouseY > this.y - hitbox/2-10 && mouseY < this.y + hitbox/2-10) {
+        this.stop();
+        this.dead = true;
+      }
+    }
   }
 }
